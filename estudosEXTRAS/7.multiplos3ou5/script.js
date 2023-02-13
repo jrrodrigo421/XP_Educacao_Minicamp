8822dd6ad7115e6ff330a3ab6d68438bdf10d246
Rodrigo Lopes <rjr89000@gmail.com>
2023-02-13 11:23:36 -0300

    exercicio7

--- estudosEXTRAS/7.multiplos3ou5/script.js
+++ estudosEXTRAS/7.multiplos3ou5/script.js
@@ -1,3 +1,8 @@
+function start(){
+  alert('7. Encontre os múltiplos de 3 ou 5. Se listarmos todos os números naturais abaixo de 10 que são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. A soma desses múltiplos é 23. Faça um programa JavaScript que encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.')
+  verificaMultiplo()
+}
+
 function verificaMultiplo(multiplo) {
   var multiplo3 = 0
   var multiplo5 = 0
@@ -16,4 +21,4 @@ function verificaMultiplo(multiplo) {
   document.write('Somatórios dos Múltiplos de 5: ' + multiplo5 + '<br>')
   document.write('Somatórios dos Múltiplos de 3 e 5: ' + parseInt(multiplo3 + multiplo5))
 }
-verificaMultiplo()
+start()
