ad0f09570e7d30c4647ed4d3fca5ee231a122772
Rodrigo Lopes <rjr89000@gmail.com>
2023-02-13 11:36:35 -0300

    exercicio8

---
+++ estudosEXTRAS/8.sequenciaFibonacci/script.js
@@ -0,0 +1,5 @@
+function start() {
+  alert('Cada novo termo na seqüência de Fibonacci é gerado adicionando os dois termos anteriores. Começando com 0 e 1, os primeiros termos serão: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ( ... ) Considerando os termos da sequência de Fibonacci a partir dos pares 0 e 1, cujos valores não excedam cinquenta mil, encontre a soma dos termos pares.')
+
+}
+start()
