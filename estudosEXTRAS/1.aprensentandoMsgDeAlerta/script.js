42e40e83b7687014265bb50cff0560c7fff38c2c
Rodrigo Lopes <rjr89000@gmail.com>
2023-02-25 11:50:58 -0300

    refatorando

--- estudosEXTRAS/1.aprensentandoMsgDeAlerta/script.js
+++ estudosEXTRAS/1.aprensentandoMsgDeAlerta/script.js
@@ -1,1 +1,9 @@
+function start(){
+  alert(` 1. Apresentando mensagens de alerta:
+
+  Escreva um programa em JavaScript que apresente 
+  uma caixa de alerta para o usuário com uma informação qualquer. `)
+}
+
+start()
 alert('testes')
--- estudosEXTRAS/2.msgAlertaPressButton/script.js
+++ estudosEXTRAS/2.msgAlertaPressButton/script.js
@@ -1,4 +1,13 @@
+function start(){
+    alert(` 2 - Apresentando mensagens de alerta pressionando botão:
+
+    Faça um programa em JavaScript em que o usuário clicará com 
+    o mouse sobre um botão e em seguida uma mensagem de alerta
+    será apresentada com uma mensagem qualquer. `)
+}
+
 function clique() {
+    
     alert('testaaaando')
 }
-// clique();
+start()
