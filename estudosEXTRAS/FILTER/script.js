feaee34a221169aa23e0ff80fcf9939ac2dee794
Rodrigo Lopes <rjr89000@gmail.com>
2023-03-12 16:37:27 -0300

    Exemplos filter

--- estudosEXTRAS/FILTER/script.js
+++ estudosEXTRAS/FILTER/script.js
@@ -13,6 +13,16 @@
 
 // console.log(numbersGreaterThan37);
 
+// Exemplo 2
+function isBigEnough(value) {
+  return value >= 10
+}
+const filtered = [12,5,8,130,44].filter(isBigEnough)
+
+// console.log(filtered)
+
+
+// Exemplo 3 
 const users = [
   { name: 'Ada Lovelace', premium: true},
   { name: 'teste', premium: false},
@@ -21,4 +31,59 @@ const users = [
 ]
 const premiumUsers = users.filter(user => user.premium)
 
-console.log(premiumUsers);
+// console.log(premiumUsers);
+
+// Exemplo 4 
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+function isPrime(num){
+  for(let i = 2; num > i; i++){
+    if(num % i === 0) {
+      return false
+    }
+  }
+  return num > 1
+}
+
+// console.log(array.filter(isPrime));
+
+// Exempo 5
+
+const arr = [
+  { id: 15},
+  { id: -1},
+  { id: 0},
+  { id: 3},
+  { id: 12.2},
+  { },
+  { id: null},
+  { id: NaN},
+  { id: "undefined"}
+]
+
+let invalidEntries = 0
+function filterByID(item) {
+  if (Number.isFinite(item.id) && item.id !== 0) {
+    return true
+  }
+  invalidEntries++
+  return false
+}
+const arrByID = arr.filter(filterByID)
+console.log("Filtered Array \n", arrByID);
+
+console.log("Number of Invalid Entries = ", invalidEntries);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ estudosEXTRAS/exercMapFilterReduce/script.js
@@ -0,0 +1,115 @@
+/*
+  01
+
+  - Gere um novo array com apenas os números ímpares do array abaixo e exiba 
+    o novo array no console.
+
+  Dica: pesquise por remainder operator (%).
+*/
+
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+
+const numImpares = randomNumbers.filter(num => (num % 2 !== 0))
+// console.log(numImpares);
+
+/*
+  02
+
+  - Exiba no console quantos números abaixo de 501 o array abaixo possui.
+*/
+
+const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+
+const number = crazyNumbers.reduce((accumulator,num) => {
+  if (num < 501){
+    const novo = [accumulator +  ','  + num].length 
+    return novo
+  }
+  return accumulator
+}, [])
+console.log(number);
+
+/*
+  03
+
+  - Gere um novo array com cada um dos números abaixo elevados ao quadrado e 
+    exiba o novo array no console.
+  
+  Dica: pesquise por exponentiation operator (**).
+*/
+
+const numbers = [5, 7, 3]
+
+/*
+  04
+
+  - Utilizando o array abaixo, gere um novo array com apenas os filmes 
+    lançados antes do ano 2000;
+  - Exiba o novo array no console.
+*/
+
+const tarantinoMovies = [
+  { name: 'Bastardos inglórios', release: 2009 },
+  { name: 'Pulp Fiction', release: 1994 },
+  { name: 'Kill Bill: Volume 2', release: 2004 },
+  { name: 'Quatro Quartos', release: 1995 },
+  { name: 'Sin City', release: 2005 },
+  { name: 'Era uma Vez em... Hollywood', release: 2019 },
+  { name: 'Django Livre', release: 2012 },
+  { name: 'Cães de Aluguel', release: 1992 },
+  { name: 'À Prova de Morte', release: 2007 },
+  { name: 'Kill Bill: Volume 1', release: 2003 }
+]
+
+/*
+  05
+
+  - Gere um novo array que contém apenas os nomes das séries abaixo;
+  - Exiba o novo array no console.    
+*/
+
+const tvShows = [
+  { name: 'Breaking Bad', releaseYear: 2008 },
+  { name: 'Mr. Robot', releaseYear: 2015 },
+  { name: 'True Detective', releaseYear: 2014 },
+  { name: 'Hannibal', releaseYear: 2013 },
+  { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
+  { name: 'House M.D.', releaseYear: 2004 },
+  { name: 'Watchmen', releaseYear: 2019 }
+]
+
+/*
+  06
+
+  - Exiba no console uma lista dos nomes dos jogos do array abaixo;
+  - A lista deve ter a formatação exemplificada abaixo do array, considerando 
+    inclusive o traço e o espaço antes de cada nome.
+  
+  Dica: para quebrar linha, você pode usar dentro da string o caractere 
+  especial \n.
+*/
+
+const cart = [
+  { name: 'Dark Souls III', price: 95.03 },
+  { name: 'Shadow of the Tomb Raider', price: 101.19 },
+  { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
+  { name: 'Resident Evil 2', price: 119.90 },
+  { name: 'Death Stranding', price: 149.99 }
+]
+
+/*
+- Nome 1
+- Nome 2
+- Nome 3
+
+Obs: 👆🏻 o objetivo do exercício é gerar a string acima. 
+
+A string deve ter exatamente a formatação acima (com traço, quebra de linha, 
+e cada item em uma linha). 
+
+console.log é apenas uma forma de você visualizar a string que você está 
+gerando. Não é o objetivo final do exercício.
+
+O objetivo final NÃO É executar um console.log para cada nome. Mas sim gerar 
+uma única string que contém uma lista exatamente como o exemplo acima. 
+*/
